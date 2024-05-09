235e7244897d09306a5ac00cdc902a46d2971edd
tyndev <17089219+tyndev@users.noreply.github.com>
2024-05-08 20:32:31 -0700

    added cors

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from typing import Union
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
 
+origins = json.loads(os.getenv("ALLOWED_ORIGINS", '[]'))
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {"message": "Hello from the backend."}
